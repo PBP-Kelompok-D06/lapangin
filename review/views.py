43f8c717cc94bbb39d1adeac5ab5a73fb144f62c
Syifa <syifaakrt@gmail.com>
2025-10-15 22:36:36 +0700

    fix fitur tambah ulasan

--- review/views.py
+++ review/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Review
 from booking.models import Lapangan as Field
+from authbooking.models import Profile
 from django.shortcuts import get_object_or_404
 
 def review_list(request, field_id):
@@ -13,7 +14,7 @@ def review_list(request, field_id):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         reviews_data = [
             {
-                "user": review.user.username,
+                "user": review.user.user.username,
                 "content": review.content,
                 "created_at": review.created_at.strftime("%d %b %Y %H:%M")
             }
@@ -28,24 +29,26 @@ def review_list(request, field_id):
         'show_navbar': True
     })
 
+
 def add_review(request, field_id):
     if request.method == "POST":
         data = json.loads(request.body)
         content = data.get("content")
         field = Field.objects.get(id=field_id)
         rating = data.get("rating")
+        user = request.user
+        profile = Profile.objects.get(user=user)
         review = Review.objects.create(
-            user=request.user,
+            user=profile,
             field=field,
             content=content,
             rating=rating,
-            created_at=data.get("created_at"),
         )
         return JsonResponse({
             "success": True,
             "message": "Review berhasil ditambahkan!",
             "review":{
-                "user": review.user.username,
+                "user": user.username,
                 "content": review.content,
                 "rating": review.rating,
                 "created_at": review.created_at,
