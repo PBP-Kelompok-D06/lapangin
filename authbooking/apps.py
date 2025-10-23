5832ba7a37b265c27afe4638aa1445a6f3623b4d
Faishal Khoiriansyah Wicaksono <faishal30112005@gmail.com>
2025-10-23 07:14:45 +0700

    cek

--- authbooking/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class AuthbookingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authbooking'
-    
-    def ready(self):
-        import authbooking.signals  # Import signals
